eed52fea24d56e8cb373f55fb239e7a17a9a5f2a
programmershine <dsrshinekim@gmail.com>
2024-05-22 17:13:48 +0900

    간단한검색v1

--- src/views/Search/SearchResult/index.jsx
+++ src/views/Search/SearchResult/index.jsx
@@ -1,49 +1,36 @@
-import React, {useEffect, useRef} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import useStore from "../../../stores/store_store"; // 경로는 실제로 사용하는 위치에 맞게 수정하세요.
 import './style.css';
-import IconBack from "../../../components/bootstrapIcon/IconBack_BS";
 import Marker from "../../../components/bootstrapIcon/Marker";
-import StarFull from "../../../components/bootstrapIcon/StarFull"; // 스타일 파일을 import
+import StarFull from "../../../components/bootstrapIcon/StarFull";
+import SearchInputBar from "../../../components/searchInputBar"; // 스타일 파일을 import
 
 function SearchResults() {
-    const searchKeyword = useStore((state) => state.searchKeyword);
     const searchResults = useStore((state) => state.searchResults); // 검색 결과 상태 추가
     const setSearchKeyword = useStore((state) => state.setSearchKeyword);
-    const inputRef = useRef(null); // 인풋 요소에 대한 ref
 
+    const { setStoreId } = useStore(); // store_store에서 setStoreId 가져오기
+    const { setStoreLatLon } = useStore(); // store_store에서 setStoreLatLon 가져오기
 
-    const handleChange = (event) => {
-        setSearchKeyword(event.target.value);
+    const navigate = useNavigate();
+    const handleResultClick = (storeId,storeName,storeLatitude,storeLongitude) => {
+        setStoreId(storeId);
+        setStoreLatLon(storeLatitude,storeLongitude);
+        setSearchKeyword(storeName);
+        navigate(`/`); // 디테일 페이지로 이동
     };
-
-    useEffect(() => {
-        // 컴포넌트가 마운트된 후에 인풋 요소에 포커스 설정
-        inputRef.current.focus();
-    }, []);
-
 
     return (
         <div>
-            <div className="search">
-            {/* 뒤로 가기 버튼 */}
-            <Link to="/" className="back-button"><IconBack style={{position:"absolute", top:"6px", left:"4px"}}/></Link>
 
-            <input
-                ref={inputRef} //ref설정
-                className="input-search"
-                type="text"
-                placeholder="장소, 음식점 검색"
-                value={searchKeyword}
-                onChange={handleChange}
-            />
-            </div>
+            <SearchInputBar/>
 
             {/* 검색 결과 표시 */}
             <div className="search-results-container">
 
                     {searchResults.map((result, index) => (
-                       <div key={index}  style={{height:'108px', paddingLeft:'12px', paddingTop:'12px', position:'relative', borderBottom:'1px solid #777'}}>
+                       <div key={index} onClick={() => handleResultClick(result.storeId, result.storeName, result.storeLatitude, result.storeLongitude )} style={{height:'108px', paddingLeft:'12px', paddingTop:'12px', position:'relative', borderBottom:'1px solid #777'}}>
                            <div style={{position:'absolute', width:'40px', height:'26px'}}>
                                <Marker/>
                            </div>
