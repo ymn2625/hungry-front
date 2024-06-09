608ae02ed6d7ba930ed013d89e28359859626a9a
btongtong <leeym2615@naver.com>
2024-06-09 22:02:31 +0900

    pull 하려고 임시 commit

--- src/views/Message/Chat/index.jsx
+++ src/views/Message/Chat/index.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useRef, useState} from "react";
 import {Stomp} from "@stomp/stompjs";
 import {getPrivateApi} from "../../../apis/privateApi";
+import defaultProfileImg from "../../../assets/images/default-profile-image.jpeg";
 
 function Message() {
     const stompClient = useRef(null);
@@ -10,14 +11,21 @@ function Message() {
         setInputValue(e.target.value);
     }
 
+    const headers = {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        'PartyId': 1
+    };
+
     const connect = () => {
         const socket = new WebSocket("ws://localhost:8088/ws");
         stompClient.current = Stomp.over(socket);
-        stompClient.current.connect({}, () => {
+        stompClient.current.connect(headers, () => {
             stompClient.current.subscribe(`/sub/parties/1`, (message) => {
                 const newMessage = JSON.parse(message.body);
                 setMessages((prevMessages) => [...prevMessages, newMessage]);
             });
+        }, (error) => {
+            console.log('Error: ' + error.body);
         });
     }
 
@@ -28,7 +36,7 @@ function Message() {
     }
 
     const getMessages = () => {
-        return getPrivateApi("http://localhost:8088/api/v1/leeym26154@naver.com/parties/1").then(getMessagesResponse);
+        return getPrivateApi("http://localhost:8088/api/v1/parties/1").then(getMessagesResponse);
     }
 
     useEffect(() => {
@@ -48,11 +56,27 @@ function Message() {
             const body = {
                 partyId: 1,
                 userEmail: "leeym26154@naver.com",
-                content: inputValue
+                content: inputValue,
+                sendTime: getSendTime(new Date())
             }
-            stompClient.current.send(`/pub/message`, {}, JSON.stringify(body));
+            stompClient.current.send(`/pub/message`, headers, JSON.stringify(body));
             setInputValue('');
         }
+    }
+
+    const getSendTime = (currentDate) => {
+        // 각 구성 요소를 가져오기
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth() + 1;
+        const day = currentDate.getDate();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+        const seconds = currentDate.getSeconds();
+
+        // 날짜와 시간을 문자열로 포맷팅
+        const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')} ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+        return formattedDate;
     }
 
     return (
@@ -61,7 +85,12 @@ function Message() {
             <button onClick={sendMessage}>전송</button>
             <div>
                 {messages.map((item, index) => (
-                    <div key={index} className="message-list-item">{item.content}</div>
+                    <div key={index} className="message-list-item">
+                        <div>{item.content}</div>
+                        <div className='sign-up-profile-img' style={{backgroundImage: `url(${item.userProfileImg ? item.userProfileImg : defaultProfileImg})`}}></div>
+                        <div>{item.userNickname}</div>
+                        <div>{item.sendTime}</div>
+                    </div>
                 ))}
             </div>
         </div>
--- src/views/Message/Rooms/index.jsx
+++ src/views/Message/Rooms/index.jsx
@@ -4,7 +4,17 @@ function Rooms(props) {
     const p1 = {
         partyProfileImg: null,
         partyTime: '점심',
-        partyTitle: '맛도리 점심 같이 먹으실 분 오세요',
+        partyTitle: '맛도실 분 오세요',
+        partyStartTime: '2024.06.02 11:30',
+        partyEndTime: '2024.06.02 13:30',
+        partyCount: 3,
+        partyLimit: 4,
+        onClickHandler: null
+    };
+    const p2 = {
+        partyProfileImg: null,
+        partyTime: '점심',
+        partyTitle: '맛도실 분 오세요 짱 맛있을 예정입니다~ ',
         partyStartTime: '2024.06.02 11:30',
         partyEndTime: '2024.06.02 13:30',
         partyCount: 3,
@@ -15,7 +25,7 @@ function Rooms(props) {
     return (
         <div>
             <PartyRoomBox {...p1} />
-            <PartyRoomBox {...p1} />
+            <PartyRoomBox {...p2} />
             <PartyRoomBox {...p1} />
             <PartyRoomBox {...p1} />
             <PartyRoomBox {...p1} />
